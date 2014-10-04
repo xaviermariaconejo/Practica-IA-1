fe712cc57904a058065ecd9a280bd71074eacea4
setzer22 <jsanchezfsms@gmail.com>
2014-10-04 14:57:09 +0200

    Generador de solució inicial random creat

--- src/IA/Desastres/Estat.java
+++ src/IA/Desastres/Estat.java
@@ -1,12 +1,14 @@
 package IA.Desastres;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**Representa la part dinàmica de l'estat del problema, és a dir, codifica una solució, i implementa els operadors de cerca.
  * @author Josep Sánchez Ferreres*/
 public class Estat {
 	
 	private ContextEstat context;
+	private Random random;
 	
 	/**Guarda, per cada helicòpter, la llista de grups que ha de rescatar*/
 	private ArrayList<ArrayList<Grupo>> helicopters;
@@ -15,10 +17,45 @@ public class Estat {
 	 * @param context El context del problema per a aquest estat.*/
 	public Estat(ContextEstat context) {
 		this.context = context;
+		random = new Random();
 		//Assumim que cada centre té el mateix nombre d'helicòpters.
 		int nH = context.getCentros().size()*context.getCentros().get(0).getNHelicopteros();
 		this.helicopters = new ArrayList<ArrayList<Grupo>>(nH);
 		for(int i = 0; i < nH; ++i) helicopters.set(i, new ArrayList<Grupo>());
+	}
+	
+	/**Genera una solució inicial aleatoria amb els grups distribuïts equitativament entre els helicòpters
+	 * @pre helicopters inicialitzat.*/
+	private void generaSolucioInicial1() {
+		int H = 0;
+		int[] l = new int[helicopters.size()]; for(int i = 0; i < l.length; ++i) l[i] = i;
+		shuffle(l);
+		int n = l.length-1;
+		Grupos grupos = context.getGrups();
+		//invariant: n apunta a l'ultim element randomitzat de la llista
+		for(Grupo g : grupos) {
+			int h = l[0]; 
+			helicopters.get(h).add(g);
+			swap(l, 0, n); 
+			n = n-1;
+			if(n == 0) {
+				n = l.length-1;
+				shuffle(l);
+			}
+		}
+	}
+	
+	/**Shuffle senzill que aleatoritza una llista d'enters fent swaps entre els elements. Implementat
+	 * amb l'algorisme de Knuth shuffle.*/
+	private void shuffle(int[] v) {
+		for(int i = 0; i < v.length; ++i) swap(v, i, random.nextInt(i+v.length-i));
+	}
+	
+	/**Swaps the elements i and j in v*/
+	private void swap(int[] v, int i, int j) {
+		int x = v[i];
+		v[i] = v[j];
+		v[j] = x;
 	}
 	
 	/**Constructor de còpia*/
