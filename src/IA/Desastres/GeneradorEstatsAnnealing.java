d1a49ea256ece02fdaa9c159829c29ba31af02de
setzer22 <jsanchezfsms@gmail.com>
2014-10-10 16:57:37 +0200

    Faltaava el generador d'estats pel simulated annealing.

---
+++ src/IA/Desastres/GeneradorEstatsAnnealing.java
@@ -0,0 +1,15 @@
+package IA.Desastres;
+
+import java.util.List;
+
+import aima.search.framework.SuccessorFunction;
+
+public class GeneradorEstatsAnnealing implements SuccessorFunction {
+
+	@Override
+	public List getSuccessors(Object state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
