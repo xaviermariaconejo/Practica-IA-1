653b36fb7914651c12f76a449f4f8158b7ac189a
luis.delicado <luis.delicado@C6S302PC55.fib.upc.es>
2014-10-22 17:46:42 +0200

    main3

--- src/IA/Desastres/Main3.java
+++ src/IA/Desastres/Main3.java
@@ -15,34 +15,46 @@ import IA.Desastres.Estat.TipusInicial;
 public class Main3 {
 	
 	private static long seed = 1234;
-	
+	private static double lamda[] = {0.001, 0.005 , 0.5, 1, 2};
+	private static int k[] = {1, 5, 20, 50, 100};
 	
 	public static void main (String[] args) {
-		Random r = new Random(1234);
-		for(int i = 0; i < 16; ++i) {
-			seed = 1234;
-			System.out.println("seed");
-			try {
-				Grupos grupos = new Grupos(100,(int) seed);
-				Centros c = new Centros(5,1,(int) seed);
-				ContextEstat cntx = new ContextEstat(grupos,c, false);
-				Estat e_ini = new Estat(cntx, seed);
-				e_ini.generaSoucioInicial(TipusInicial.GREEDY);
-				System.out.println("El greedy triga un total de: "+e_ini.getTempsViatges()+"min");
-				Problem problem = new Problem(e_ini, new GeneradorEstatsAnnealing(), new EstatFinal(), new FuncioHeuristica1());
-				Search search = new SimulatedAnnealingSearch();
-				long time = System.currentTimeMillis();
-				SearchAgent agent = new SearchAgent(problem, search);
-				System.out.println((System.currentTimeMillis() - time));
-				int rot, mov, mou_n, swap_g, swap_v;
-				rot = mov = mou_n = swap_g = swap_v = 0;
-				List<Estat> l = search.getPathStates();
-				if(l.size() > 0) System.out.println("La solució millorada és de: "+l.get(l.size()-1).getTempsViatges()+"min");
-				//printActions(agent.getActions());
-				//printInstrumentation(agent.getInstrumentation());
-			} catch (Exception e) {
-				e.printStackTrace();
+		
+		for(int i = 0; i < lamda.length; ++i) {
+			for(int j = 0; j < k.length; ++j) {
+			Random r = new Random(1234);
+			int n = 15;
+			double sum = 0;
+			for(int t = 0; t < 15; ++t) {
+				seed = r.nextLong();
+				//System.out.println("seed");
+				try {
+					Grupos grupos = new Grupos(100,(int) seed);
+					Centros c = new Centros(5,1,(int) seed);
+					ContextEstat cntx = new ContextEstat(grupos,c, false);
+					Estat e_ini = new Estat(cntx, seed);
+					e_ini.generaSoucioInicial(TipusInicial.GREEDY);
+					//System.out.println("El greedy triga un total de: "+e_ini.getTempsViatges()+"min");
+					Problem problem = new Problem(e_ini, new GeneradorEstatsAnnealing(), new EstatFinal(), new FuncioHeuristica1());
+					Search search = new SimulatedAnnealingSearch(10000,100,k[j],lamda[i]);
+					long time = System.currentTimeMillis();
+					SearchAgent agent = new SearchAgent(problem, search);
+					//System.out.println((System.currentTimeMillis() - time));
+					int rot, mov, mou_n, swap_g, swap_v;
+					rot = mov = mou_n = swap_g = swap_v = 0;
+					List<Estat> l = search.getPathStates();
+					if(l.size() > 0) sum += l.get(l.size()-1).getTempsViatges();
+					else --n;
+					//printActions(agent.getActions());
+					//printInstrumentation(agent.getInstrumentation());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
+			if(n != 0) System.out.print((sum/(double)n)+"\t");
+			else System.out.print("NULL\t");
+			}
+			System.out.println();
 		}
 	}
 	
